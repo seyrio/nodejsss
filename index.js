e84cc42ed161ee19534a477b3080f3aebe878e75
seyrio <agrawal3h2@gmail.com>
2023-09-29 14:38:46 +0530

    initialization 1.1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello from the project!");
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const pathLocation = path.resolve();
+  res.sendFile(path.join(pathLocation, "index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server is working");
+});
